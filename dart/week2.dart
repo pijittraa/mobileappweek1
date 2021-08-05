09d179e816d1c95634c2d13d4a9de8098add1ac9
pijittratrisu <pijittratrisutha@gmail.com>
2021-08-05 17:17:19 +0700

    เพิ่มคอมเม้น

--- dart/week2.dart
+++ dart/week2.dart
@@ -6,16 +6,18 @@ void main() {
 }
 //เขียนแบบ Arrow Function
 String callGen(int year) => (year >= 1996) ? "Gen Z" : "Gen Y";
-
+//           สีที่ต้องการเพิ่ม       
 int callColor([String newColor = "White"]) {
 
   //                        0       1      2
   List<dynamic> color = ["BLACK", "GREEN", "BLUE"];
+  // เพิ่มสี
   color.add(newColor);
   for (var item in color) {
     print(item);
   }
+  // ตัวนับจำนวนสี
   return color.length;
 }
-
+//            ชื่อเว็บ           เลข port       Protocal                         
 void callWeb(String web , {int port = 80, String protocal = "http"})=>print("$protocal $web $port");
